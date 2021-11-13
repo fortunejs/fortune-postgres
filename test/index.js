4c5a9c6b6f21b238411204411436c858aaef0bbf
Dali Zheng <d@liwa.li>
2021-11-13 15:42:05 -0800

    bump versions

--- test/index.js
+++ test/index.js
@@ -3,10 +3,9 @@ const adapter = require('../lib')
 
 testAdapter(adapter, {
   url: 'postgres://postgres@localhost:5432/fortune_test',
-  isNative: true,
+  // isNative: true,
   primaryKeyType: 'integer',
   useForeignKeys: true,
-  // isNative: true,
   generatePrimaryKey: () => Math.floor(Math.random() * Math.pow(2, 16)),
   typeMap: {
     user: 'users',
